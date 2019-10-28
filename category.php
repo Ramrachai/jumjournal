13f779ba34669f165648c151428efb4f70c7455f
Ramrachai <ramrachaim@gmail.com>
2019-10-28 21:31:55 +0600

    category page and tag page done

--- category.php
+++ category.php
@@ -19,87 +19,112 @@ $container = get_theme_mod( 'understrap_container_type' );
 $category = get_queried_object();
 ?>
 
-<div class="category-banner" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/banners/lake-bg-1x.jpg);">
-	<div class="<?php echo esc_attr( $container ); ?>">
-		<div class="row">
-			<div class="col-md-12">
-				<h1 class="category-name">
-					<?php echo $category->name; ?>
-				</h1>
+<div class="category-banner"
+    style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/cat-bg.webp);">
+    <div class="<?php echo esc_attr( $container ); ?>">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="category-name">
+                    <?php echo $category->name; ?>
+                </h1>
 
-				<?php
+                <?php
 					if ( $category->description ) { 
 				?>
-					<p class="category-description"> 
-						<?php echo $category->description; ?>
-					</p>
-				<?php
+                <p class="category-description">
+                    <?php echo $category->description; ?>
+                </p>
+                <?php
 					};
 
 					if ( $category->category_count > 1 ) { 
 				?>
-					<p class="total-posts"> 
-						<?php echo $category->category_count; ?> posts in this category
-					</p>
-				<?php
+                <p class="total-posts">
+                    <?php echo $category->category_count; ?> posts in this category
+                </p>
+                <?php
 					};
 				?>
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="wrapper" id="archive-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+        <div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+            <div class="col-md-2"></div>
+            <div class="col-md-8 col-12">
+                <main class="site-main post-style-one" id="main">
 
-			<main class="site-main post-style-one" id="main">
+                    <div class="row">
 
-				<div class="row">
+                        <?php if ( have_posts() ) : ?>
 
-					<?php if ( have_posts() ) : ?>
+                        <?php /* Start the Loop */ ?>
+                        <?php while ( have_posts() ) : the_post(); ?>
 
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
+                        <?php
 
-							<?php
+			/*
+			* Include the Post-Format-specific template for the content.
+			* If you want to override this in a child theme, then include a file
+			* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			*/
+			get_template_part( 'loop-templates/content', get_post_format() );
+			?>
 
-							/*
-							* Include the Post-Format-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							*/
-							get_template_part( 'loop-templates/content', get_post_format() );
-							?>
+                        <?php endwhile; ?>
 
-						<?php endwhile; ?>
+                        <?php else : ?>
 
-					<?php else : ?>
+                        <?php get_template_part( 'loop-templates/content', 'none' ); ?>
 
-						<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+                        <?php endif; ?>
 
-					<?php endif; ?>
+                    </div>
 
-				</div>
+                </main><!-- #main -->
 
-			</main><!-- #main -->
+                <!-- The pagination component -->
+                <?php understrap_pagination(); ?>
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+            </div> <!-- .row -->
 
-		</div> <!-- .row -->
+            <div class='col-md-2 ' style='padding: 0 8px 0 8px ;'>
+                <ul class='category-list'>
+                    <?php wp_list_categories(); ?>
+                </ul>
 
-	</div><!-- #content -->
+                <p class='tag-list-title'>Tags</p>
+                <ul class='tag-list'>
+                    <?php
+                    $tags = get_tags();
+                    if ( $tags ) :
+                        foreach ( $tags as $tag ) : ?>
+                    <li> <a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>"
+                            title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a>
+                    </li>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                </ul>
 
-	</div><!-- #archive-wrapper -->
+            </div>
+        </div>
+
+
+    </div><!-- #content -->
+
+
+</div>
+
+</div><!-- #archive-wrapper -->
+
+<?php get_template_part('global-templates/footer-one'); ?>
 
 <?php get_footer(); ?>
